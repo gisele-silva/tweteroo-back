9fbb2fc16bdfa940be1ebe7c9271c376dd0b5d5f
Gisele <gisele_s@hotmail.com>
2023-04-09 18:58:15 -0300

    servidor

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express()
+const PORT = 5000;
+
+app.listen(PORT, () => {console.log(`Servidor rodando na porta ${PORT}`)})
+
+app.get("/tweets", (req, res) => {
+    ASASASD
+})
+
+app.post("/tweets", (req, res) => {
+
+})
+
+app.post("/sign-up", (req, res) => {
+    
+})
